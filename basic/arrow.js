9784a37f61e266d8328f911b67fcd8a9bc1bc71b
Chandrakant Sharma <143052477+Chandrakantsharma91@users.noreply.github.com>
2024-02-23 05:31:10 +0000

    iifi

--- basic/arrow.js
+++ basic/arrow.js
@@ -37,5 +37,26 @@ const chai = () =>{
 
 // const addTwo =(num1 , num2) => ({userName : "chandra"})
 
-console.log(addTwo(3,5));
+//console.log(addTwo(3,5));
+
+   // Concept of IFFI  which is imidetly invoced function
+
+(function chai(){
+    console.log(`DB CONNECTED`);
+})();  //here terminator is used because it don,t know where to end
+
+//()()  global scope ke pollution ko htane ke liye iffi ka use krte h
+
+( function code(){
+    // It is called named iffi
+    console.log(`DB 2 CONNECTED`);
+}) ();
+
+((name) =>{
+    console.log(`DB CONNECTED TWO ${name}`);
+})('hitesh')
+
+ 
+
+
 
